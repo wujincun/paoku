523e66828197aebf5973af33b2ed85589f89d2c2
wujincun <1769453779@qq.com>
2016-11-16 18:14:51 +0800

    提交： 2、$.when用于图片的全部加载为生效 3、画小人的跑步姿势

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ var paoku ={
         /* bgAdditionHeight: 0, //bgAddition是跑道附加素材
          bgAdditionDistance: 0,*/
         runner: {},
-        /* frameCount: 0,
-         swipeLock: true,*/
+        frameCount: 0,
+         /*swipeLock: true,*/
         totalDistance: 0,
         runwayLength: 0,
         bgSpeed: 0,
@@ -31,7 +31,7 @@ var paoku ={
     },
     init: function() {
         var _this = this;
-        //加载完图片???
+        //加载完图片后render
         var imgs = [
             './img/countdown_bg_1.png',
             './img/countdown_bg_2.png',
@@ -54,11 +54,11 @@ var paoku ={
             }
 
         }
-        /* this.intCons.totalDistance = this.intCons.runwayLength = w * 15; //跑道长度是宽度15倍
-         this.intCons.baseSpeed = this.intCons.speedNormal = this.intCons.bgSpeed = Math.round(this.intCons.runwayLength / 1500); //25秒完成游戏，一秒60帧
-         this.speedFast = Math.round(this.speedNormal * 7 / 4);
-         this.speedSlow = Math.round(this.speedNormal / 4);*/
-
+        //设置初始值
+         _this.intCons.totalDistance = _this.intCons.runwayLength = _this.intCons.w * 15; //跑道长度是宽度15倍
+         _this.intCons.baseSpeed = _this.intCons.speedNormal = _this.intCons.bgSpeed = Math.round(_this.intCons.runwayLength / 1500); //25秒完成游戏，一秒60帧
+         _this.intCons.speedFast = Math.round(_this.intCons.speedNormal * 7 / 4);
+         _this.intCons.speedSlow = Math.round(_this.intCons.speedNormal / 4);
     },
     render: function () {
         var _this = this;
@@ -81,10 +81,10 @@ var paoku ={
     },
     renderBg:function (ctx) {
         var _this = this;
-        var bg = new Image();
-        bg.src = './img/new_bg.jpg';
-        bg.onload= function () {
-            ctx.drawImage(bg, 0, 0, _this.intCons.w, _this.intCons.h);
+        _this.intCons.bg = new Image();
+        _this.intCons.bg.src = './img/new_bg.jpg';
+        _this.intCons.bg.onload= function () {
+            ctx.drawImage(_this.intCons.bg, 0, 0, _this.intCons.w, _this.intCons.h);
         };
     },
     renderBlock:function (ctx) {
@@ -92,19 +92,28 @@ var paoku ={
     },
     renderRunner:function(ctx){
         var _this = this;
-        var runner = _this.intCons.runner;
         var w = this.intCons.w;
         var h = this.intCons.h;
         //按照图片尺寸设定人物宽高
-        runner.size = [w * 0.2, w * 0.2 * 190 / 130];
-        runner.centerPositon = (w - runner.size[0]) / 2;
-        runner.positon = [runner.centerPositon, h - runner.size[1]];
+        _this.intCons.runner.size = [w * 0.2, w * 0.2 * 190 / 130];
+        _this.intCons.runner.centerPositon = (w - _this.intCons.runner.size[0]) / 2;
+        _this.intCons.runner.positon = [_this.intCons.runner.centerPositon, h - _this.intCons.runner.size[1]];
+        _this.intCons.runner.animateState = 1;
 
-        /*var runnerImg = ['./img/person_nm_1','./img/person_nm_2'];
+        var runnerImg = {
+            normal: './img/person_nm_'
+        };
 
-        this.runner.normal[1].onload = function () {
-            ctx.drawImage(_this.runner.normal[1], _this.runner.positon[0], _this.runner.positon[1], _this.runner.size[0], _this.runner.size[1]);
-        }*/
+        for (var key in runnerImg) {
+            _this.intCons.runner[key] = new Object();
+            for (var i = 1; i < 3; i++) {
+                _this.intCons.runner[key][i] = new Image();
+                _this.intCons.runner[key][i].src = runnerImg[key] + i + '.png';
+            }
+        }
+        _this.intCons.runner.normal[1].onload = function () {
+            ctx.drawImage(_this.intCons.runner.normal[1], _this.intCons.runner.positon[0], _this.intCons.runner.positon[1],_this.intCons.runner.size[0], _this.intCons.runner.size[1]);
+        }
     },
     renderListener:function (ctx) {
         var _this = this;
@@ -122,12 +131,55 @@ var paoku ={
         }, 1000);
     },
     run:function (ctx) {
+        var _this = this;
+        function animateRun () {
+            var curTime = Date.now();
+           /* if (_this.lastTime > 0) {
+                _this.bgSpeed = _this.baseSpeed * (60 * (curTime - _this.lastTime) / 1000);
+            }
+            _this.lastTime = curTime;*/
+
+            //注意顺序，先画背景，再画终点线，再画刻度，再画障碍物，最后画人物
+            ctx.clearRect(0, 0, _this.w, _this.h);
+
+            _this.rollBg(ctx);
+            _this.frameCount++;
+            _this.drawRunner(ctx);
+            //障碍物
+
+
+           /* if (_this.frameCount % 5 == 0) {
+                _this.collisionTest();
+            }
+            _this.runwayLength -= _this.bgSpeed;
+            if (_this.runwayLength + _this.runner.size[1] <= 0) {
+                _this.endRun();
+                return false;
+            }
+
+            _this.rafId = window.requestAnimationFrame(animateRun);*/
+        }
+        animateRun();
         //背景
 
         //人
 
         //
 
+    },
+    rollBg:function (ctx) {
+        var _this = this;
+        _this.intCons.bgDistance += _this.intCons.bgSpeed;
+        if (_this.intCons.bgDistance < _this.intCons.h) {
+            ctx.drawImage(_this.intCons.bg, 0, _this.intCons.bgDistance, _this.intCons.w, _this.intCons.h);
+        }
+    },
+    drawRunner:function (ctx) {
+        var _this = this;
+        if (this.frameCount % 10 == 0) {
+            _this.intCons.runner.animateState == 1 ? _this.intCons.runner.animateState = 2 : _this.intCons.runner.animateState = 1;
+        }
+        ctx.drawImage(_this.intCons.runner.normal[_this.intCons.runner.animateState], _this.intCons.runner.positon[0], _this.intCons.runner.positon[1], _this.intCons.runner.size[0], _this.intCons.runner.size[1]);
     },
     countTime:function () {
 
